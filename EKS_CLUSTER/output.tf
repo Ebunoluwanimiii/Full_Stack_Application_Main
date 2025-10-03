b15958d9d55aa21836ebed41824f649c8ee3310e
Ebunoluwanimiii <ebunoluwaoloyede@gmail.com>
2025-10-03 19:12:10 +0100

    Update output.tf

--- EKS_CLUSTER/output.tf
+++ EKS_CLUSTER/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.ekscluster.id
+  value = aws_eks_cluster.BloggingApp.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.ekscluster.id
+  value = aws_eks_node_group.BloggingApp.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.ekscluster_vpc.id
+  value = aws_vpc.BloggingApp_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.ekscluster_subnet[*].id
+  value = aws_subnet.BloggingApp_subnet[*].id
 }
