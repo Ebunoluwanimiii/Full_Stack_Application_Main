3d4cb41b2092ec2ae5a604977c67a123c3ba7466
Ebunoluwanimiii <ebunoluwaoloyede@gmail.com>
2025-10-03 19:11:06 +0100

    Update main.tf

--- EKS_CLUSTER/main.tf
+++ EKS_CLUSTER/main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_vpc" "ekscluster_vpc" {
+resource "aws_vpc" "BloggingApp_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "ekscluster-vpc"
+    Name = "BloggingApp-vpc"
   }
 }
 
-resource "aws_subnet" "ekscluster_subnet" {
+resource "aws_subnet" "BloggingApp_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.ekscluster_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.ekscluster_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.BloggingApp_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.BloggingApp_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["eu-west-2a", "eu-west-2b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "ekscluster-subnet-${count.index}"
+    Name = "BloggingApp-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "ekscluster_igw" {
-  vpc_id = aws_vpc.ekscluster_vpc.id
+resource "aws_internet_gateway" "BloggingApp_igw" {
+  vpc_id = aws_vpc.BloggingApp_vpc.id
 
   tags = {
-    Name = "ekscluster-igw"
+    Name = "BloggingApp-igw"
   }
 }
 
-resource "aws_route_table" "ekscluster_route_table" {
-  vpc_id = aws_vpc.ekscluster_vpc.id
+resource "aws_route_table" "BloggingApp_route_table" {
+  vpc_id = aws_vpc.BloggingApp_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ekscluster_igw.id
+    gateway_id = aws_internet_gateway.BloggingApp_igw.id
   }
 
   tags = {
-    Name = "ekscluster-route-table"
+    Name = "BloggingApp-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.ekscluster_subnet[count.index].id
-  route_table_id = aws_route_table.ekscluster_route_table.id
+  subnet_id      = aws_subnet.BloggingApp_subnet[count.index].id
+  route_table_id = aws_route_table.BloggingApp_route_table.id
 }
 
-resource "aws_security_group" "ekscluster_cluster_sg" {
-  vpc_id = aws_vpc.ekscluster_vpc.id
+resource "aws_security_group" "BloggingApp_cluster_sg" {
+  vpc_id = aws_vpc.BloggingApp_vpc.id
 
   ingress {
       from_port   = 0
@@ -67,12 +67,12 @@ resource "aws_security_group" "ekscluster_cluster_sg" {
   }
 
   tags = {
-    Name = "ekscluster-cluster-sg"
+    Name = "BloggingApp-cluster-sg"
   }
 }
 
-resource "aws_security_group" "ekscluster_node_sg" {
-  vpc_id = aws_vpc.ekscluster_vpc.id
+resource "aws_security_group" "BloggingApp_node_sg" {
+  vpc_id = aws_vpc.BloggingApp_vpc.id
 
   ingress {
     from_port   = 0
@@ -89,25 +89,25 @@ resource "aws_security_group" "ekscluster_node_sg" {
   }
 
   tags = {
-    Name = "ekscluster-node-sg"
+    Name = "BloggingApp-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "ekscluster" {
-  name     = "ekscluster-cluster"
-  role_arn = aws_iam_role.ekscluster_cluster_role.arn
+resource "aws_eks_cluster" "BloggingApp" {
+  name     = "BloggingApp-cluster"
+  role_arn = aws_iam_role.BloggingApp_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.ekscluster_subnet[*].id
-    security_group_ids = [aws_security_group.ekscluster_cluster_sg.id]
+    subnet_ids         = aws_subnet.BloggingApp_subnet[*].id
+    security_group_ids = [aws_security_group.BloggingApp_cluster_sg.id]
   }
 }
 
-resource "aws_eks_node_group" "ekscluster" {
-  cluster_name    = aws_eks_cluster.ekscluster.name
-  node_group_name = "ekscluster-node-group"
-  node_role_arn   = aws_iam_role.ekscluster_node_group_role.arn
-  subnet_ids      = aws_subnet.ekscluster_subnet[*].id
+resource "aws_eks_node_group" "BloggingApp" {
+  cluster_name    = aws_eks_cluster.BloggingApp.name
+  node_group_name = "BloggingApp-node-group"
+  node_role_arn   = aws_iam_role.BloggingApp_node_group_role.arn
+  subnet_ids      = aws_subnet.BloggingApp_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -119,12 +119,12 @@ resource "aws_eks_node_group" "ekscluster" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.ekscluster_node_sg.id]
+    source_security_group_ids = [aws_security_group.BloggingApp_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "ekscluster_cluster_role" {
-  name = "ekscluster-cluster-role"
+resource "aws_iam_role" "BloggingApp_cluster_role" {
+  name = "BloggingApp-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -142,13 +142,13 @@ resource "aws_iam_role" "ekscluster_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "ekscluster_cluster_role_policy" {
-  role       = aws_iam_role.ekscluster_cluster_role.name
+resource "aws_iam_role_policy_attachment" "BloggingApp_cluster_role_policy" {
+  role       = aws_iam_role.BloggingApp_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "ekscluster_node_group_role" {
-  name = "ekscluster-node-group-role"
+resource "aws_iam_role" "BloggingApp_node_group_role" {
+  name = "BloggingApp-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -166,17 +166,17 @@ resource "aws_iam_role" "ekscluster_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "ekscluster_node_group_role_policy" {
-  role       = aws_iam_role.ekscluster_node_group_role.name
+resource "aws_iam_role_policy_attachment" "BloggingApp_node_group_role_policy" {
+  role       = aws_iam_role.BloggingApp_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "ekscluster_node_group_cni_policy" {
-  role       = aws_iam_role.ekscluster_node_group_role.name
+resource "aws_iam_role_policy_attachment" "BloggingApp_node_group_cni_policy" {
+  role       = aws_iam_role.BloggingApp_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "ekscluster_node_group_registry_policy" {
-  role       = aws_iam_role.ekscluster_node_group_role.name
+resource "aws_iam_role_policy_attachment" "BloggingApp_node_group_registry_policy" {
+  role       = aws_iam_role.BloggingApp_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
