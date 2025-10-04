a575136426be2a660d37825ca1512be41ee56cee
Ebunoluwanimiii <ebunoluwaoloyede@gmail.com>
2025-10-04 04:05:53 +0100

    Update SecurityConfig.java

--- src/main/java/com/example/twitterapp/config/SecurityConfig.java
+++ src/main/java/com/example/twitterapp/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.twitterapp.config;
 
-import com.example.twitterapp.security.CustomUserDetailsService;
+import com.example.twitterapp.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
